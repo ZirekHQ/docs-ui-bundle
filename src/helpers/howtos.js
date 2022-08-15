31c84eca3fde60c4e8324d72733806dc4abb8155
darknao <darknao@fedoraproject.org>
2022-08-15 12:51:28 +0200

    Add howTo link on navbar if defined in antora playbook

---
+++ src/helpers/howtos.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = (editUrl, options) => {
+  var ret = ''
+  if (!editUrl) return ret
+  let howtos = options.data.root.site.keys.howtos
+  if (!howtos) return ret
+  howtos = JSON.parse(howtos)
+
+  let howto = ''
+  if (editUrl.includes('://github.com/')) {
+    if (howtos.github) howto = howtos.github
+  }
+  if (editUrl.includes('://pagure.io/')) {
+    if (howtos.gitlab) howto = howtos.pagure
+  }
+  if (editUrl.includes('://gitlab.com/')) {
+    if (howtos.gitlab) howto = howtos.gitlab
+  }
+
+  if (howto) {
+    const page = options.data.root.contentCatalog.resolvePage(howto.link)
+    if (page) ret = options.fn({ title: howto.title, link: page.pub.url })
+  }
+
+  return ret
+}
